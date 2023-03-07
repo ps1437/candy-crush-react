0dc2c7a05480c4ba41c5a156469fcbc93dd7f414
Praveen Soni <praveen.soni@jda.com>
2023-03-07 15:47:45 +0530

    candru crush

---
+++ src/data/Data.js
@@ -0,0 +1,17 @@
+import blueCandy from "../images/blue-candy.png";
+import greenCandy from "../images/green-candy.png";
+import orangeCandy from "../images/orange-candy.png";
+import purpleCandy from "../images/purple-candy.png";
+import redCandy from "../images/red-candy.png";
+import yellowCandy from "../images/red-candy.png";
+
+export const CANDIES = [
+  blueCandy,
+  orangeCandy,
+  purpleCandy,
+  redCandy,
+  yellowCandy,
+  greenCandy,
+];
+
+export const BOARD_WIDTH = 8;
